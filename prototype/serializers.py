4ae0f6366ff9336041e3c4c002124e71d9ef6051
GutesTod <jefflol003@gmail.com>
2024-03-29 15:42:51 +0500

    modified:   prototype/serializers.py

--- prototype/serializers.py
+++ prototype/serializers.py
@@ -1,14 +1,3 @@
-from rest_framework import routers, serializers, viewsets
-from .models import User
-
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['name', 'surname', 'hashed_password']
-  
-#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\      
-        
 from rest_framework import serializers
 from .models import User
 
