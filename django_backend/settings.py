171653801aa62d5d237ff6c53cac559278c5f0a2
roki <shamilsatlykov@gmail.com>
2024-03-29 21:37:07 +0500

    added corse

--- django_backend/settings.py
+++ django_backend/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'prototype',
     'tournament',
+    'corsheaders',
     'rest_framework'
 ]
 
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -142,3 +144,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'prototype.User'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8080',
+]
